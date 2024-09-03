ddd535c4652698982145d895fc5ce85bfb0164e3
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-09-03 21:23:10 +0600

    chore: remove zitadel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { authOptions } from "@/collections/auth";
 import { getServerSession } from "next-auth";
-import LoginButton from "./modules/root/LoginButton";
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
-	return <main>{session ? <p>Authenticated</p> : <LoginButton />}</main>;
+	return <main>Hello</main>;
 }
