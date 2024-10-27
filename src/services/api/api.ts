916faf2a7870de1e92b53c5d078774421ccaf7f4
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-10-27 17:31:18 +0600

    fix: generated api not working due to client headers not being sent on request

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -3,22 +3,46 @@ import { Auth } from "./Auth";
 import { HttpClient } from "./http-client";
 import { Organizations } from "./Organizations";
 import { User } from "./User";
+import { isBrowser } from "@/utils/is-browser";
+import {
+	AxiosHeaders,
+	type InternalAxiosRequestConfig,
+} from "axios";
 
 class ApiService {
-  auth: Auth;
-  user: User;
-  organizations: Organizations;
+	auth: Auth;
+	user: User;
+	organizations: Organizations;
 
-  constructor() {
-    const httpClient = new HttpClient({
-      baseURL: Env.API_URI,
-      withCredentials: true,
-      responseType: "json",
-    });
-    this.auth = new Auth(httpClient);
-    this.user = new User(httpClient);
-    this.organizations = new Organizations(httpClient);
-  }
+	constructor() {
+		const httpClient = new HttpClient({
+			baseURL: Env.API_URI,
+			withCredentials: true,
+			responseType: "json",
+		});
+		if (!isBrowser()) {
+			httpClient.instance.interceptors.request.use(async (config) => {
+				const globalHeaders = await import("next/headers").then((mod) =>
+					mod.headers().entries(),
+				);
+
+				const headers = new AxiosHeaders(config.headers);
+
+				for (const [key, value] of globalHeaders) {
+					headers[key] = value;
+				}
+
+				return {
+					...config,
+					headers,
+				} satisfies InternalAxiosRequestConfig;
+			});
+		}
+
+		this.auth = new Auth(httpClient);
+		this.user = new User(httpClient);
+		this.organizations = new Organizations(httpClient);
+	}
 }
 
 export const apiService = new ApiService();
